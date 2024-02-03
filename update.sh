8ed17b565ce03b507f8732962572960af681bcc8
cachiusa <nhat.dogpro@outlook.com>
2024-02-03 04:00:58 +0000

    Add authors

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+while IFS=',' read -r name repourl remotebranch forceupdate ; do
+  if [ "$name" != "name" ]; then
+    echo "$name $repourl $remotebranch"
+    set -x
+    if git show-ref --quiet refs/heads/"$name"; then
+      git checkout "$name"
+    else
+      git checkout --orphan "$name"
+      git rm -rf . > /dev/null 2>&1
+    fi
+
+    if [ "$forceupdate" != "yes" ]; then
+      git pull "$repourl" "$remotebranch"
+    else
+      git fetch "$repourl" "$remotebranch"
+      git reset --hard FETCH_HEAD
+    fi
+    git push -u origin "$name"
+  fi
+done < index.csv
